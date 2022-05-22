7baaa95a9a67013bfd9f5ed7baf2a7e36a3fa90a
Xzyra Lapid <83936947+xzyra14@users.noreply.github.com>
2022-05-23 05:35:55 +0800

    Add files via upload

---
+++ MOD4_Activity1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MOD4_Activity1 : ContentPage
+    {
+        public MOD4_Activity1()
+        {
+            InitializeComponent();
+        }
+    }
+}
